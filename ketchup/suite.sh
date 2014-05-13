5a833394a1905eeb4ed7388575681159bf5d5951
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2014-05-12 17:28:21 -0700

    remove hesitate from ketchup suite

--- ketchup/suite.sh
+++ ketchup/suite.sh
@@ -6,7 +6,6 @@ ginkgo -- -algorithm=reserve_n_best -maxBiddingPool=20 -maxConcurrent=20
 ginkgo -- -algorithm=pick_best -maxBiddingPool=20 -maxConcurrent=20
 ginkgo -- -algorithm=random -maxBiddingPool=20 -maxConcurrent=20
 ginkgo -- -algorithm=pick_among_best -maxBiddingPool=20 -maxConcurrent=20
-ginkgo -- -algorithm=hesitate -maxBiddingPool=20 -maxConcurrent=20
 
 ginkgo -- -algorithm=all_revote -maxBiddingPool=100 -maxConcurrent=20
 ginkgo -- -algorithm=all_reserve -maxBiddingPool=100 -maxConcurrent=20
@@ -14,7 +13,6 @@ ginkgo -- -algorithm=reserve_n_best -maxBiddingPool=100 -maxConcurrent=20
 ginkgo -- -algorithm=pick_best -maxBiddingPool=100 -maxConcurrent=20
 ginkgo -- -algorithm=random -maxBiddingPool=100 -maxConcurrent=20
 ginkgo -- -algorithm=pick_among_best -maxBiddingPool=100 -maxConcurrent=20
-ginkgo -- -algorithm=hesitate -maxBiddingPool=100 -maxConcurrent=20
 
 ginkgo -- -algorithm=all_revote -maxBiddingPool=20 -maxConcurrent=100
 ginkgo -- -algorithm=all_reserve -maxBiddingPool=20 -maxConcurrent=100
@@ -22,7 +20,6 @@ ginkgo -- -algorithm=reserve_n_best -maxBiddingPool=20 -maxConcurrent=100
 ginkgo -- -algorithm=pick_best -maxBiddingPool=20 -maxConcurrent=100
 ginkgo -- -algorithm=random -maxBiddingPool=20 -maxConcurrent=100
 ginkgo -- -algorithm=pick_among_best -maxBiddingPool=20 -maxConcurrent=100
-ginkgo -- -algorithm=hesitate -maxBiddingPool=20 -maxConcurrent=100
 
 ginkgo -- -algorithm=all_revote -maxBiddingPool=20 -maxConcurrent=1000
 ginkgo -- -algorithm=all_reserve -maxBiddingPool=20 -maxConcurrent=1000
@@ -30,7 +27,6 @@ ginkgo -- -algorithm=reserve_n_best -maxBiddingPool=20 -maxConcurrent=1000
 ginkgo -- -algorithm=pick_best -maxBiddingPool=20 -maxConcurrent=1000
 ginkgo -- -algorithm=random -maxBiddingPool=20 -maxConcurrent=1000
 ginkgo -- -algorithm=pick_among_best -maxBiddingPool=20 -maxConcurrent=1000
-ginkgo -- -algorithm=hesitate -maxBiddingPool=20 -maxConcurrent=1000
 
 ginkgo -- -algorithm=all_revote -maxBiddingPool=100 -maxConcurrent=100
 ginkgo -- -algorithm=all_reserve -maxBiddingPool=100 -maxConcurrent=100
@@ -38,7 +34,6 @@ ginkgo -- -algorithm=reserve_n_best -maxBiddingPool=100 -maxConcurrent=100
 ginkgo -- -algorithm=pick_best -maxBiddingPool=100 -maxConcurrent=100
 ginkgo -- -algorithm=random -maxBiddingPool=100 -maxConcurrent=100
 ginkgo -- -algorithm=pick_among_best -maxBiddingPool=100 -maxConcurrent=100
-ginkgo -- -algorithm=hesitate -maxBiddingPool=100 -maxConcurrent=100
 
 ginkgo -- -algorithm=all_revote -maxBiddingPool=100 -maxConcurrent=1000
 ginkgo -- -algorithm=all_reserve -maxBiddingPool=100 -maxConcurrent=1000
@@ -46,4 +41,3 @@ ginkgo -- -algorithm=reserve_n_best -maxBiddingPool=100 -maxConcurrent=1000
 ginkgo -- -algorithm=pick_best -maxBiddingPool=100 -maxConcurrent=1000
 ginkgo -- -algorithm=random -maxBiddingPool=100 -maxConcurrent=1000
 ginkgo -- -algorithm=pick_among_best -maxBiddingPool=100 -maxConcurrent=1000
-ginkgo -- -algorithm=hesitate -maxBiddingPool=100 -maxConcurrent=1000
